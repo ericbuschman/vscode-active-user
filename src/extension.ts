c3d889f88dc359b1a0f9291fa6d45df9bdcf3936
Eric Buschman <Eric.Buschman@amerihome.com>
2020-03-05 07:33:53 -0800

    Working version

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,16 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+let myStatusBarItem: vscode.StatusBarItem;
+export function activate() {
+	const myCommandId = 'statusbar.showCurrentUser';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+	// create a new status bar item that we can now manage
+	let myStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+	myStatusBarItem.command = myCommandId;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-active-user" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	// update status bar item once at start
+	let userName = require("os").userInfo().username;
+	myStatusBarItem.text = `User: ${userName}`;
+	myStatusBarItem.show();
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
